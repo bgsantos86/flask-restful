556bf9835eaecc0b3de4aae4b265b243384ccd54
Bruno Santos <brunog86@gmail.com>
2019-07-17 23:10:51 -0300

    Add PUT and DEL in courses resource

--- resources/courses.py
+++ resources/courses.py
@@ -52,7 +52,8 @@ class CourseList(Resource):
     def post(self):
         args = self.reqparse.parse_args()
         course = models.Course.create(**args)
-        return add_reviews(course)
+        return (add_reviews(course), 201, 
+                {'Location': url_for('resourses.courses.course', id=course.id)})
 
 class Course(Resource):
     def __init__(self):
@@ -76,11 +77,18 @@ class Course(Resource):
     def get(self, id):
         return add_reviews(course_or_404(id))
 
+    @marshal_with(course_fields)
     def put(self, id):
-        return jsonify({'title': 'Python Basics'})
+        args = self.reqparse.parse_args()
+        query = models.Course.update(**args).where(models.Course.id==id)
+        query.execute()
+        return (add_reviews(models.Course.get(models.Course.id==id)), 200, 
+                {'Location': url_for('resources.courses.course', id=id)})
 
     def delete(self, id):
-        return jsonify({'title': 'Python Basics'})
+        query = models.Course.delete().where(models.Course.id==id)
+        query.execute()
+        return ('', 204, {'Location': url_for('resources.courses.courses')})
 
 courses_api = Blueprint('resources.courses', __name__)
 api = Api(courses_api)
